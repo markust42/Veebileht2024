fde9222c7503302a58f148e7902e2b4171f7455f
Annabel Priks <annabel.priks@gmail.com>
2024-12-02 20:51:41 +0200

    Tegime koos Markusega pildikarusellid korda, liigutasime mapsi ja panime kontaktaadressid

--- javascriptid/main.js
+++ javascriptid/main.js
@@ -1,17 +1,25 @@
-let slideIndex = 1;
-showSlides(slideIndex);
+document.addEventListener("DOMContentLoaded", function() {
+  const carousels = document.querySelectorAll('.pildikarusell');
+  carousels.forEach(carousel => {
+      const id = carousel.id;
+      window[id] = { slideIndex: 1 };
+      showSlides(1, id);
+  });
+});
 
-function plusSlides(n) {
-  showSlides(slideIndex += n);
+function plusSlides(n, carouselId) {
+  window[carouselId].slideIndex += n;
+  showSlides(window[carouselId].slideIndex, carouselId);
 }
 
-function showSlides(n) {
+function showSlides(n, carouselId) {
   let i;
-  let slides = document.getElementsByClassName("pilt");
-  if (n > slides.length) {slideIndex = 1}
-  if (n < 1) {slideIndex = slides.length}
+  let carousel = document.getElementById(carouselId);
+  let slides = carousel.getElementsByClassName("pilt");
+  if (n > slides.length) { window[carouselId].slideIndex = 1 }
+  if (n < 1) { window[carouselId].slideIndex = slides.length }
   for (i = 0; i < slides.length; i++) {
-    slides[i].style.display = "none";
+      slides[i].style.display = "none";
   }
-  slides[slideIndex-1].style.display = "block";
+  slides[window[carouselId].slideIndex - 1].style.display = "block";
 }
